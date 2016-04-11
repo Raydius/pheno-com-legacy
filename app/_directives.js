ce36cebfc15393cd816c1ddee3f3d54a7df0d4e3
An-Ni Wang <anwang@phenomenon.com>
2016-04-10 22:21:01 -0700

    use require to place image in jade
    Former-commit-id: 98729c70381429798e8322aa824955754f53f5b4

--- app/_directives.js
+++ app/_directives.js
@@ -110,7 +110,7 @@ angular.module('phenoCom').directive('updateTitle', ['$rootScope', '$timeout',
         return {
             link: function(scope, element) {
 
-                var listener = function(event, toState) {
+                var listener = function(event, toState) { 
 
                     var title = 'Default Title';
                     if (toState.data && toState.data.pageTitle) title = toState.data.pageTitle;
@@ -118,6 +118,7 @@ angular.module('phenoCom').directive('updateTitle', ['$rootScope', '$timeout',
                     $timeout(function() {
                         element.text(title);
                     }, 0, false);
+
                 };
 
                 $rootScope.$on('$stateChangeSuccess', listener);
