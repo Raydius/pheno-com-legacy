567f94d09787d8443b0380d9b8c8509bc2936645
Ray Dollete <rjdollete@gmail.com>
2016-05-17 11:31:11 -0700

    Fix vimeo bug

--- app/_config.js
+++ app/_config.js
@@ -1,6 +1,15 @@
-/**
- * Created by rdollete on 4/1/16.
- */
 import 'npm/owl-carousel-2/owl.carousel';
 
 var copy = require('app/copy.js');
+
+angular.module('phenoCom').config(function($sceDelegateProvider) {
+
+
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'http://tech.phenomenonstaging.com/**',
+    'https://player.vimeo.com/**',
+    'https://vimeo.com/**'
+  ]);
+
+});
--- app/phenomenon_work.js
+++ app/phenomenon_work.js
@@ -14,6 +14,26 @@ require('angular-cookies');
 require('angular-messages');
 require('angular-snap');
 
+$('body').show();
+$(window).on('hashchange', function(e){
+    $('body').hide();
+});
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-51195233-1', 'phenomenon.com');
+ga('send', 'pageview');
+
 angular.module('phenoCom',[
     uirouter,
     ngAnimate,
