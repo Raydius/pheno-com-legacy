83d2363bb291afd4c2e3ad39eece17b5c7cc33f1
Ray Dollete <ray@raydollete.com>
2016-11-08 17:19:17 -0800

    Implement greenhouse API on new jobs index page

--- app/_controllers.js
+++ app/_controllers.js
@@ -66,29 +66,55 @@ angular.module('phenoCom').controller('homeController', function($state, $scope,
 angular.module('phenoCom').controller('jobsController', function($scope, $state, $http) {
 
     $scope.jobs = {
-        openPositions: []
+        departments: []
     };
 
     $http({
         method: 'GET',
-        url: 'https://api.greenhouse.io/v1/boards/phenomenon/embed/jobs'
+        url: 'https://api.greenhouse.io/v1/boards/phenomenon/embed/departments'
     }).then(function (response) {
 
-        var jobs = response.data.jobs;
+        var departments = response.data.departments;
 
-        for(var i=0, len = jobs.length; i < len; i++) {
+        // step through all departments
+        for(var i=0, len = departments.length; i < len; i++) {
 
-            var job = jobs[i];
+            var depJobs = departments[i].jobs;
+            var openPositions = [];
 
-            // job must have a LinkedIn URL in order to be listed on the jobs page
-            if(job.metadata[0].value) {
-                var position = {
-                    title: job.title,
-                    url: job.metadata[0].value
-                };
-                $scope.jobs.openPositions.push(position);
+            // step through all jobs in the department
+            for (var n=0, jlen = depJobs.length; n < jlen; n++) {
+
+                var job = depJobs[n];
+
+                // job must have a LinkedIn URL in order to be listed on the jobs page
+                if(job.metadata[0].value) {
+
+                    var position = {
+                        title: job.title,
+                        url: job.metadata[0].value,
+                        location: job.location.name
+                    };
+                    openPositions.push(position);
+                }
+
             }
+
+            // if there were jobs in this department...
+            if(openPositions.length > 0) {
+
+                // push department to scope
+                var department = {
+                    'name': departments[i].name,
+                    'openPositions': openPositions
+                };
+
+                $scope.jobs.departments.push(department);
+            }
+
         }
+
+        console.log($scope.jobs);
     });
 
 });
