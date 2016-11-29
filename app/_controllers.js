dc7196fd8538fb8250bf621e4f5cb52139e18ba5
Jessie Pimenta <jpimenta@phenomenon.com>
2016-11-29 15:08:56 -0800

    - added form validation for name, email, and phone number - added a simple controller to hold some logic

--- app/_controllers.js
+++ app/_controllers.js
@@ -281,3 +281,17 @@ angular.module('phenoCom').controller('blogPostController', function($scope, $st
     });
 
 });
+
+angular.module('phenoCom').controller('applicationFormController', function($scope) {
+
+		// function to submit the form after all validation has occurred			
+		$scope.submitForm = function() {
+
+			// check to make sure the form is completely valid
+			if ($scope.userForm.$valid) {
+				alert('our form is amazing');
+			}
+
+		};
+
+	});
