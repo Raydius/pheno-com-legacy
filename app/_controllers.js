5524f9be2f3330941f59e9ca2163a28f58cbc478
Jessie Pimenta <jpimenta@phenomenon.com>
2016-12-06 16:07:13 -0800

    fixed extra space under video and padding

--- app/_controllers.js
+++ app/_controllers.js
@@ -368,11 +368,18 @@ angular.module('phenoCom').controller('scrollController', function(){
 })
 
 angular.module('phenoCom').controller('holidayController', function(){
+
+  var holidayvideo = document.getElementById('holidayvideo')
+  $('.maincontent').css('padding','0')
   $('#holidayvideo').hide();
 
   $('#holidaygif').click(function(){
     $('#holidaygif').hide();
     $('#holidayvideo').show()
-    $('#holidayvideo').play()
+    holidayvideo.play()
+  })
+
+  $('a').click(function(){
+  $('.maincontent').css('padding','0 150px')
   })
 })
