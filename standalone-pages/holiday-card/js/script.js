fa1a61db282a43b7d76d8147a42c4b868d0b4f93
Jessie Pimenta <jpimenta@phenomenon.com>
2016-12-19 17:44:56 -0800

    updated the final functionality and gif

--- standalone-pages/holiday-card/js/script.js
+++ standalone-pages/holiday-card/js/script.js
@@ -1,6 +1,19 @@
 
 var holidayGif = document.getElementById("holidayGif");
 var holidayVideo = document.getElementById('holidayVideo');
+var playbutton = document.getElementById('playbutton');
+var windowWidth = window.windowWidth;
+
+function playVideoFromButton() {
+  var playbutton = document.getElementById('playbutton');
+  var holidayGif = document.getElementById("holidayGif");
+  var holidayVideo = document.getElementById('holidayVideo');
+  holidayGif.style.display = "none";
+  holidayVideo.style.display = 'block';
+  holidayVideo.play();
+  playbutton.style.display = 'none'
+}
+
 
 function hideGifAndPlayVideo(){
 var holidayGif = document.getElementById("holidayGif");
@@ -8,7 +21,7 @@ var holidayVideo = document.getElementById('holidayVideo');
 holidayGif.style.display = "none";
 holidayVideo.style.display = 'block';
 holidayVideo.play();
-
+playbutton.style.display = 'none'
 
 };
 
@@ -23,3 +36,26 @@ holidayVideo.play();
   });
 
 }
+
+$(document).ready(function(){
+
+  if($(window).width() < 475) {
+    $('#holidayGif').attr('src','unnamed.gif').animate({
+"position": "absolute",
+"minHeight": "inherit",
+"padding": "0",
+"margin": "0 auto",
+"width": "100%",
+"height": "auto",
+"backgroundSize": "cover",
+zIndex: "1",
+    },10)
+console.log('true')
+
+  $
+}
+  // holidayGif.style.display = 'none';
+  // holidayVideo.style.display = 'block';
+  // holidayVideo.play();
+  // playbutton.style.display = 'none'
+})
