5df180b4724034ed09873cb451e34271ce713002
Ray Dollete <ray@raydollete.com>
2017-03-01 12:14:30 -0800

    Form upload mid-saving state

--- app/_controllers-careers.js
+++ app/_controllers-careers.js
@@ -40,23 +40,6 @@ angular.module('phenoCom').controller('jobController', function($scope, $statePa
 	// get jobId from URL parameter
 	$scope.jobId = $stateParams.jobId;
 
-	// a jobId of 0 means it is a non-specific position
-	/*if($scope.jobId == 0) {
-
-		// static data for non-specific job
-		$scope.data = {
-			content: 'Description of general job...',
-			departments: [
-				{
-					id: 0,
-					name: 'Unspecified'
-				}
-			],
-			title: 'Open Application'
-		};
-
-	}*/
-
 	// otherwise get the information from Greenhouse for this job
 	//else {
 		$scope.applyRoute = 'job.application';
@@ -67,7 +50,7 @@ angular.module('phenoCom').controller('jobController', function($scope, $statePa
 			url: $scope.apiUrl + '/jobs/job/' + $scope.jobId + '/'
 		}).then(function (response) {
 			$scope.data = response.data;
-			console.log(response.data);
+
 		});
 
 		$scope.renderHtml = function(html_code) {
@@ -85,6 +68,19 @@ angular.module('phenoCom').controller('jobController', function($scope, $statePa
  * Controller for Job-Specific Application Form
  */
 angular.module('phenoCom').controller('jobApplicationController', function($scope, $state, $stateParams, $location, $http, envService) {
+
+	// allow submit status (prevent multiple submits while form is uploading)
+	$scope.allowSubmit = function(allow) {
+
+		if(allow) {
+			$scope.disableSubmit = false;
+			$scope.submitText = 'Submit Application';
+		}
+		else {
+			$scope.disableSubmit = true;
+			$scope.submitText = 'Saving...';
+		}
+	};
 
 	// initial placeholder data
 	$scope.data = {
@@ -129,78 +125,85 @@ angular.module('phenoCom').controller('jobApplicationController', function($scop
 
 	$scope.submitForm = function() {
 
+		// only process form if the submit is not disabled
+		if($scope.disableSubmit == false) {
 
-		if($scope.userForm.$valid) {
+			$scope.allowSubmit(false);
 
-			console.log('form is valid');
+			if ($scope.userForm.$valid) {
 
-			// compile form fields into data object
-			let data = ({
-				'id': $scope.jobId,
-				'first_name': $scope.user.firstname,
-				'last_name': $scope.user.lastname,
-				'email': $scope.user.email,
-				'phone': $scope.user.phone,
-				'website': $scope.user.website,
-				'linkedin': $scope.user.linkedin,
-				'department': $scope.selectedDepartment
-			});
+				// compile form fields into data object
+				let data = ({
+					'id': $scope.jobId,
+					'first_name': $scope.user.firstname,
+					'last_name': $scope.user.lastname,
+					'email': $scope.user.email,
+					'phone': $scope.user.phone,
+					'website': $scope.user.website,
+					'linkedin': $scope.user.linkedin,
+					'department': $scope.selectedDepartment
+				});
 
-			// create FormData object from form fields and optional file attachment
-			let fd = new FormData();
-			fd.append('data', JSON.stringify(data));
-			fd.append('resume', $scope.resume);
+				// create FormData object from form fields and optional file attachment
+				let fd = new FormData();
+				fd.append('data', JSON.stringify(data));
+				fd.append('resume', $scope.resume);
 
-			$http({
-				method: 'POST',
-				url: $scope.apiUrl + '/jobs/apply/',
-				data: fd,
-				headers: {'Content-Type': undefined},
-				transformRequest: angular.identity
-			}).then(function(response) {
+				$http({
+					method: 'POST',
+					url: $scope.apiUrl + '/jobs/apply/',
+					data: fd,
+					headers: {'Content-Type': undefined},
+					transformRequest: angular.identity
+				}).then(function (response) {
 
-				// if successful, go to thank you page
-				if(response.data.success == "Candidate saved successfully") {
-					$state.go('thanks');
-				}
-
-				// if not (either proxy API unreachable, or Greenhouse API unreachable)
-				else {
-					console.log(response);
-					// handle errors
-				}
-
-				// $('.resume-attached-hidden').show()
-
-
-			}, function(data, status, headers, config) {
-
-				// handle errors
-
-			});
-
-		}
-		else {
-
-			console.log('form is invalid');
-
-			angular.forEach($scope.userForm, function(value, key) {
-
-				// add field-level error messages after attempted submit even if user has never entered those fields
-				if (typeof value === 'object') {
-					let el = $scope.userForm[key];
-
-					if(el.$invalid) {
-						el.attempted = true;
+					// if successful, go to thank you page
+					if (response.data.success == "Candidate saved successfully") {
+						$state.go('thanks');
 					}
-				}
 
-			});
+					// if not (either proxy API unreachable, or Greenhouse API unreachable)
+					else {
+						console.log(response);
+						// handle errors
+						$scope.allowSubmit(true);
+					}
 
-			$scope.userForm.$setPristine();
-			$scope.userForm.$setUntouched();
+					// $('.resume-attached-hidden').show()
 
-			alert('Please fill out all of the required fields.');
+
+				}, function (data, status, headers, config) {
+
+					// handle errors
+					$scope.allowSubmit(true);
+
+				});
+
+			}
+			else {
+
+				$scope.allowSubmit(true);
+
+				angular.forEach($scope.userForm, function (value, key) {
+
+					// add field-level error messages after attempted submit even if user has never entered those fields
+					if (typeof value === 'object') {
+						let el = $scope.userForm[key];
+
+						if (el.$invalid) {
+							el.attempted = true;
+						}
+					}
+
+				});
+
+				$scope.userForm.$setPristine();
+				$scope.userForm.$setUntouched();
+
+				alert('Please fill out all of the required fields.');
+			}
 		}
 	};
+
+	$scope.allowSubmit(true);
 });
