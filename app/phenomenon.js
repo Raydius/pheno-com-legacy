ad3230c6ea79d1b2304f17001ee1f5106e53bbb0
Ray Dollete <ray@raydollete.com>
2017-03-28 15:02:01 -0700

    Remove references to jquery.filer (no longer needed)

--- app/phenomenon.js
+++ app/phenomenon.js
@@ -9,7 +9,6 @@ import ngAnimate from 'angular-animate';
 import 'angular-socialshare';
 import 'sticky-kit/dist/sticky-kit.min.js';
 import 'ui-router-metatags/dist/ui-router-metatags.js';
-//import 'jquery.filer/js/jquery.filer.js';
 
 require('angular-cookies');
 require('angular-messages');
